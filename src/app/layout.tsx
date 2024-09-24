a621306d8d4c012a36d075f430e3a20b31860adc
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2024-09-24 23:01:03 +0530

    metadata added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+
 import "./globals.css";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import { Header } from "@/components/Header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 
-
+export const metadata = {
+  title: "Adarsh Tiwari | Personal Portfolio",
+  description: 
+    "Adarsh Tiwari is a software engineer based in India. He is passionate about building web applications."
+}
 
 export default function RootLayout({
   children,
